5b06dce0702d1352ab4c9d72321558590dcd17ac
Thomas Künneth <thomas@thomaskuenneth.com>
2016-11-07 14:37:02 +0100

    Added a pure Android version. Spot the similarities.

---
+++ Android/app/src/main/java/com/thomaskuenneth/temperatureconverter/MainActivity.java
@@ -0,0 +1,139 @@
+package com.thomaskuenneth.temperatureconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final Model model = new Model();
+
+    private RadioGroup tempSrc, tempDesti;
+    private EditText input;
+    private Button calculate;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tempSrc = (RadioGroup) findViewById(R.id.tempSrc);
+        tempSrc.setOnCheckedChangeListener((group, checked) -> {
+            switch (checked) {
+                case R.id.srcCelsius:
+                    model.setInUnit(Model.TEMPERATURE_UNIT.DEGREES_CELSIUS);
+                    break;
+                case R.id.srcFahrenheit:
+                    model.setInUnit(Model.TEMPERATURE_UNIT.DEGREES_FAHRENHEIT);
+                    break;
+                case R.id.srcKelvin:
+                    model.setInUnit(Model.TEMPERATURE_UNIT.KELVIN);
+                    break;
+                default:
+                    Log.e(TAG, "Unexpected input unit");
+            }
+        });
+        tempDesti = (RadioGroup) findViewById(R.id.tempDesti);
+        tempDesti.setOnCheckedChangeListener((group, checked) -> {
+            switch (checked) {
+                case R.id.destiCelsius:
+                    model.setOutUnit(Model.TEMPERATURE_UNIT.DEGREES_CELSIUS);
+                    break;
+                case R.id.destiFahrenheit:
+                    model.setOutUnit(Model.TEMPERATURE_UNIT.DEGREES_FAHRENHEIT);
+                    break;
+                case R.id.destiKelvin:
+                    model.setOutUnit(Model.TEMPERATURE_UNIT.KELVIN);
+                    break;
+                default:
+                    Log.e(TAG, "Unexpected output unit");
+            }
+        });
+        calculate = (Button) findViewById(R.id.calculate);
+        calculate.setOnClickListener((view) -> {
+            model.setInTemperature(Double.valueOf(input.getText().toString()));
+            model.calculateOutTemperature();
+            updateOutTemperatureFromModel();
+        });
+        input = (EditText) findViewById(R.id.input);
+        input.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                updateCalculateButton();
+            }
+        });
+        input.setOnEditorActionListener((view, actionId, event) -> {
+            calculate.performClick();
+            return true;
+        });
+        result = (TextView) findViewById(R.id.result);
+        updateInputUnitFromModel();
+        updateOutputUnitFromModel();
+        updateInTemperatureFromModel();
+        updateOutTemperatureFromModel();
+        updateCalculateButton();
+    }
+
+    private void updateCalculateButton() {
+        calculate.setEnabled(input.getText().length() > 0);
+    }
+
+    private void updateInputUnitFromModel() {
+        model.calculateOutTemperature();
+        switch (model.getInUnit()) {
+            case DEGREES_CELSIUS:
+                tempSrc.check(R.id.srcCelsius);
+                break;
+            case DEGREES_FAHRENHEIT:
+                tempSrc.check(R.id.srcFahrenheit);
+                break;
+            case KELVIN:
+                tempSrc.check(R.id.srcKelvin);
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected input unit");
+        }
+    }
+
+    private void updateOutputUnitFromModel() {
+        switch (model.getOutUnit()) {
+            case DEGREES_CELSIUS:
+                tempDesti.check(R.id.destiCelsius);
+                break;
+            case DEGREES_FAHRENHEIT:
+                tempDesti.check(R.id.destiFahrenheit);
+                break;
+            case KELVIN:
+                tempDesti.check(R.id.destiKelvin);
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected output unit");
+        }
+    }
+
+    private void updateInTemperatureFromModel() {
+        Double inTemperature = model.getInTemperature();
+        input.setText(inTemperature == null ? "" : inTemperature.toString());
+    }
+
+    private void updateOutTemperatureFromModel() {
+        result.setText(model.getOutTemperatureAsString());
+    }
+}
